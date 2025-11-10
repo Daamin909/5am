c57ede656be785801538558edecde41d69fe8a03
daamin909 <ashai.daamin@gmail.com>
2025-11-10 05:49:21 +0000

    added ping group

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ load_dotenv()
 CHANNEL_ID = "C087L4MB2V9"
 USER_ID = "U07GLQY6UN4"
 SLACK_TOKEN=os.getenv("SLACK_TOKEN")
-TEST_MODE = True
+TEST_MODE = False
 # ----------------------------------------
 
 client = WebClient(token=SLACK_TOKEN)
@@ -125,10 +125,10 @@ def post_message_and_thread(text, thread_text):
 
 def build_stats_text(week_days, month_days,):
     stats_text = (
-        f"*Early Morning Message Stats*\n"
-        f"• This week: *{len(week_days)}* day(s)\n"
-        f"• This month: *{len(month_days)}* day(s)\n"
-        f"*Dates (Month)*:\n"
+        f"*wake up before 5am stats*\n"
+        f"• ts week: *{len(week_days)}* days :aga:\n"
+        f"• ts month: *{len(month_days)}* days :aga:\n"
+        f"*the dates for ts month*:\n"
     )
     if month_days:
         stats_text += "\n".join(f"• {d}" for d in formatted_dates(month_days))
@@ -144,11 +144,15 @@ def post_stats_if_early():
         messages_today = fetch_messages(today_start - timedelta(seconds=1))
         early_today = get_early_days(messages_today, before_hour=5)
         if not early_today:
-            main_text = "<@U07GLQY6UN4> dumbahh couldn't even wake up before 5 :loll:"
+            main_text = "<@U07GLQY6UN4> dumbahh couldn't even wake up before 5 :icant:"
+            one_more = "<!subteam^S09RZ5VC6UU> bully this dumbahh for not waking up :sob:"
             main = client.chat_postMessage(channel=CHANNEL_ID, text=main_text)
+            thread_ts = main["ts"]
+            try:
+                thread_msg = client.chat_postMessage(channel=CHANNEL_ID, thread_ts=thread_ts, text=one_more)           
+            except: 
+                raise
             return
-
-
         week_start = now_ist - timedelta(days=7)
         month_start = now_ist - timedelta(days=30)
 
@@ -158,7 +162,7 @@ def post_stats_if_early():
         week_days = get_early_days(week_msgs, before_hour=5)
         month_days = get_early_days(month_msgs, before_hour=5)
 
-        main_text = "<@U07GLQY6UN4> bro thinks he tuff by waking up before 5 am :icant:"
+        main_text = "<@U07GLQY6UN4> bro thinks he tuff by waking up before 5 am :agahappi:"
         thread_text = build_stats_text(week_days, month_days)
 
         post_message_and_thread(main_text, thread_text)
@@ -184,4 +188,4 @@ if __name__ == "__main__":
     if TEST_MODE:
         post_stats_if_early()
     else:
-        ist_scheduler_loop(target_hour=10, target_minute=53)
+        ist_scheduler_loop(target_hour=9, target_minute=11)
